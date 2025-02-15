58c58a2b19394e75a77b407e4d3c11eaacdd473c
Shakil Hussain <shakil.hussain2022@gmail.com>
2025-02-15 15:57:50 -0700

    init

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame
 from pygame.locals import *
 import sys
+import pygbag
 
 
 # Initialize Pygame
 pygame.init()
 
 # Set the screen dimensions and create the screen object
-screen_width = 900
-screen_height = 600
+screen_width = 1366
+screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("HackTheBomb")
+# icon = pygame.image.load("icon.png")
+# pygame.display.set_icon(icon)
 
 def main_screen():
     """Display the main screen with emojis"""
@@ -22,6 +26,10 @@ def main_screen():
             elif event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # Fill the screen with the background image
+            background_image = pygame.image.load("graphics/bg1.png")
+            screen.blit(background_image, (0, 0))
 
         # Update the display
         pygame.display.flip()
