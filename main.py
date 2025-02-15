5189de0b711c6fe7499fbdf361334ecb2f438df3
Shakil Hussain <shakil.hussain2022@gmail.com>
2025-02-15 16:36:07 -0700

    edited main screen

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ screen_width = 1366
 screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("HackTheBomb")
-# icon = pygame.image.load("icon.png")
-# pygame.display.set_icon(icon)
+icon = pygame.image.load("graphics/icon.png")
+pygame.display.set_icon(icon)
+
+# Main Text
+main_text = pygame.image.load("graphics/main_text.png")
 
 def main_screen():
     """Display the main screen with emojis"""
@@ -28,8 +31,12 @@ def main_screen():
                 sys.exit()
 
             # Fill the screen with the background image
-            background_image = pygame.image.load("graphics/bg1.png")
+            background_image = pygame.image.load("graphics/placeholder_bg.png")
             screen.blit(background_image, (0, 0))
+
+            # Get dimensions of main_text image
+            main_text_rect = main_text.get_rect(center=(screen_width // 2, screen_height // 4))
+            screen.blit(main_text, main_text_rect)
 
         # Update the display
         pygame.display.flip()
