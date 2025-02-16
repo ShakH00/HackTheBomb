ce00fa3796ce2ced9af19a5a3ade9975a0dbd7da
Yahya Asmara <yahya16005@gmail.com>
2025-02-15 17:22:57 -0700

    Removed useless files (I accidentally committed the wrong folder of all my learning python files) - basically this game was made, but right now is a backup in local storage

--- BombDefusalGameBackup/apni dictionary.py
+++
@@ -1,9 +0,0 @@
-# simple dictionary
-# only enter keyword set,name,sirname
-dic={"set":"1,2,3",
-     "name":"nitesh",
-    "sirname": "raut"
-     }
-# print("enter dictionary name ");
-n=input("enter dictionary name ")
-print(dic[n])
